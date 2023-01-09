01fb73249970feb042820d3fdde3ac742c300efd
AllaYukhym <yukhymenko.alla@gmail.com>
2023-01-09 19:36:20 +0100

    add works files

---
+++ src/components/App/App.styled.js
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  padding: 30px;
+`;
+
+export const Title = styled.h1`
+  font-size: 30px;
+`;
+
+export const Subtitle = styled.h2`
+  font-size: 28px;
+`;
