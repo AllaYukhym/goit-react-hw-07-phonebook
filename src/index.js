df47351c6d52136485bf0e2412bc9cb2c66c7b5d
AllaYukhym <yukhymenko.alla@gmail.com>
2023-01-11 15:55:26 +0100

    delete persistor

--- src/index.js
+++ src/index.js
@@ -1,10 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
-import { store, persistor } from './redux/store';
+import { store } from './redux/store';
 import { App } from './components/App/App';
 import { GlobalStyle } from './components/GlobalStyle';
-// import { PersistGate } from 'redux-persist/integration/react';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
